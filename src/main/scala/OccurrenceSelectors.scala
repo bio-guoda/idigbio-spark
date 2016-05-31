d01582974b782702becdbd68ff06bc15926bcd47
Jorrit Poelen <jhpoelen@gmail.com>
2016-05-30 17:18:27 -0700

    instantiate shapes once

--- src/main/scala/OccurrenceSelectors.scala
+++ src/main/scala/OccurrenceSelectors.scala
@@ -3,20 +3,32 @@
 object OccurrenceSelectors {
 
   def taxonSelector(config: OccurrenceSelector): OccurrenceExt => Boolean = {
-    x => config.taxonSelector.split("\\|").intersect(x.taxonPath.split("\\|")).nonEmpty
+    val selectedTaxa: Array[String] = config.taxonSelector.split("\\|")
+    if (selectedTaxa.length > 0) {
+      x => selectedTaxa.intersect(x.taxonPath.split("\\|")).nonEmpty
+    } else {
+      x => false
+    }
+  }
+
+  def traitSelector(config: OccurrenceSelector): OccurrenceExt => Boolean = {
+    x => true
   }
 
   def geoSpatialSelector(config: OccurrenceSelector): OccurrenceExt => Boolean = {
-    x => {
-      SpatialFilter.parseWkt(config.wktString) match {
-        case Some(area) => SpatialFilter.valuesInArea(Seq(x.lat, x.lng), area)
-        case _ => false
+    SpatialFilter.parseWkt(config.wktString) match {
+      case Some(area) => {
+        (x: OccurrenceExt) => SpatialFilter.valuesInArea(Seq(x.lat, x.lng), area)
+      }
+      case _ => {
+        x => false
       }
     }
   }
 
   def all(config: OccurrenceSelector): OccurrenceExt => Boolean = {
-    x => Seq(taxonSelector(config), geoSpatialSelector(config)).forall(_ (x))
+    x => Seq(traitSelector _, taxonSelector _, geoSpatialSelector _)
+      .forall(_ (config)(x))
   }
 }
 
--- src/test/scala/OccurrenceSelectors$Test.scala
+++ src/test/scala/OccurrenceSelectors$Test.scala
@@ -13,24 +13,24 @@ class OccurrenceSelectors$Test extends FlatSpec with Matchers {
     selectors(notMatching) should be(false)
   }
 
-//  ignore "a event time selector" should "include matching occurrences" in {
-//    val selectors = OccurrenceSelectors.all(OccurrenceSelector("Plantae", "ENVELOPE(4,5,52,50)", "eventTime > 2016-01-01 datetime"))
+//  "a event time selector" should "include matching occurrences" in {
+//    val selectors = OccurrenceSelectors.traitSelector(OccurrenceSelector(traitSelector = "eventTime > 2016-01-01 datetime"))
 //    val matching = OccurrenceExt("51", "4.1", "Plantae", "", 0L, "bla", 0L, 0L)
 //    selectors(matching) should be(false)
 //
-//    val invertedSelectors = OccurrenceSelectors.all(OccurrenceSelector("Plantae", "ENVELOPE(4,5,52,50)", "eventTime < 2016-01-01 datetime"))
+//    val invertedSelectors = OccurrenceSelectors.traitSelector(OccurrenceSelector(traitSelector = "eventTime < 2016-01-01 datetime"))
 //    invertedSelectors(matching) should be(true)
 //  }
 //
 //  "a source selector" should "include matching occurrences" in {
-//    val selectors = OccurrenceSelectors.all(OccurrenceSelector("Plantae", "ENVELOPE(4,5,52,50)", "source == otherSource"))
+//    val selectors = OccurrenceSelectors.traitSelector(OccurrenceSelector(traitSelector = "source == otherSource"))
 //    val matching = OccurrenceExt("51", "4.1", "Plantae", "", 0L, "someSource", 0L, 0L)
 //    selectors(matching) should be(false)
 //
-//    val invertedSelectors = OccurrenceSelectors.all(OccurrenceSelector("Plantae", "ENVELOPE(4,5,52,50)", "source == someSource"))
+//    val invertedSelectors = OccurrenceSelectors.traitSelector(OccurrenceSelector(traitSelector = "source == someSource"))
 //    invertedSelectors(matching) should be(true)
 //
-//    val combinedSelectors = OccurrenceSelectors.all(OccurrenceSelector("Plantae", "ENVELOPE(4,5,52,50)", "source == {otherSource,someSource}"))
+//    val combinedSelectors = OccurrenceSelectors.all(OccurrenceSelector(traitSelector = "source == {otherSource,someSource}"))
 //    combinedSelectors(matching) should be(true)
 //  }
 
