c4e3f80b9db1e54aea3d7d1a950cc60a7db78078
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-04 17:29:51 -0700

    backup hourly; keep days

--- backup_queries_and_subscriptions.sh
+++ backup_queries_and_subscriptions.sh
@@ -3,11 +3,12 @@ OUTPUT_DIR=$1
 CASSANDRA_HOME=/home/int/apache-cassandra-2.1.13/
 
 function backup {
-	filename=$OUTPUT_DIR$1-`date -u +"%Y%m%dT%H%M%SZ"`.csv
+	filename=$OUTPUT_DIR$1-`date -u +"%Y%m%d"`.csv
 	command="COPY $1 TO STDOUT;"
 	echo $command
 	$CASSANDRA_HOME/bin/cqlsh -e "$command" > $filename
 	cp $filename $OUTPUT_DIR$1-current.csv
+	cp $filename $OUTPUT_DIR$1-`date -u +"%Hh"`.csv
 }
 
 echo "backup starting..."
@@ -15,4 +16,5 @@ backup effechecka.monitors
 backup effechecka.selector
 backup effechecka.checklist_registry
 backup effechecka.occurrence_collection_registry
+backup effechecka.subscriptions
 echo "backup complete."
