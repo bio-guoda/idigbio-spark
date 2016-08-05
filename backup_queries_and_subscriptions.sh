4d0f9d4f56d7a70dd9255483afd30e548e7644c4
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-04 17:12:31 -0700

    add backup script

---
+++ backup_queries_and_subscriptions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+OUTPUT_DIR=$1
+CASSANDRA_HOME=/home/int/apache-cassandra-2.1.13/
+
+function backup {
+	filename=$OUTPUT_DIR$1.`date -u +"%Y%m%dT%H%M%SZ"`.csv
+	command="COPY $1 TO STDOUT;"
+	echo $command
+	$CASSANDRA_HOME/bin/cqlsh -e "$command" > $filename
+	cp $filename $1.csv
+}
+
+echo "backup starting..."
+backup effechecka.monitors
+backup effechecka.selector
+backup effechecka.checklist_registry
+backup effechecka.occurrence_collection_registry
+echo "backup complete."
