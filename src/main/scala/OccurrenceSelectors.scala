d63853dc7b1e5441a3f6c1557b6a025166457b9e
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-19 09:59:18 -0700

    add error info; throw exception on malformed trait selector

--- src/main/scala/OccurrenceSelectors.scala
+++ src/main/scala/OccurrenceSelectors.scala
@@ -22,7 +22,7 @@ object OccurrenceSelectors {
       TraitSelectorParser.parse(TraitSelectorParser.config, config.traitSelector) match {
         case TraitSelectorParser.Success(selector, _) => selector
         case failure: TraitSelectorParser.NoSuccess => {
-          Console.err.println(s"traitSelector from [$config] failed to parse: [$failure.msg]")
+          scala.sys.error(s"traitSelector from [$config] failed to parse: [$failure.msg]")
           selectNever
         }
       }
